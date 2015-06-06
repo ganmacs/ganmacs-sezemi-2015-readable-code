e3de52169eb7054e44f2e29bd53f61fa123eb4ec
ganmacs <ganmacs@gmail.com>
2015-06-06 11:58:48 +0900

    done spec5

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -8,13 +8,37 @@ class Recipe
   end
 
   def all_with_id
-    recipes.map.with_index(1) { |recipe, i| "#{i}: #{recipe}" }
+    if exist_recipe_id?
+      recipe_by_id(recipe_id)
+    else
+      recipes.map.with_index(1) { |recipe, i| "#{i}: #{recipe}" }
+    end
   end
 
   private
 
   def recipes
     @recipes ||= load_recipe_file
+  end
+
+  def recipe_size
+    recipes.size
+  end
+
+  def recipe_by_id(id)
+    recipes[id]
+  end
+
+  def recipe_id
+    ARGV[1].to_i - 1 if valid_recipe_id?
+  end
+
+  def valid_recipe_id?
+    ARGV[1].to_i > 0 or raise "#{ARGV[1]} is not valid id"
+  end
+
+  def exist_recipe_id?
+    !!ARGV[1]
   end
 
   def load_recipe_file
