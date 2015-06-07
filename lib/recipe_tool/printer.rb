c0066b90f9506722ea5b2744b1c99dc9cf6fe93a
ganmacs <ganmacs@gmail.com>
2015-06-07 12:25:50 +0900

    spec 10

--- lib/recipe_tool/printer.rb
+++ lib/recipe_tool/printer.rb
@@ -3,8 +3,8 @@ require 'recipe_tool/manager'
 module RecipeTool
   class Printer
     def call
-      manager.all_recipes_with_user.each do |recipe_with_user|
-        puts "ユーザ名: #{recipe_with_user[:user_name]}" if recipe_with_user[:user_name]
+      manager.all_recipes_with_user.each_with_index do |recipe_with_user, i|
+        puts "ユーザ: #{i}: #{recipe_with_user[:user_name]}" if recipe_with_user[:user_name]
         recipe_with_user[:recipes].each do |recipe|
           puts "#{recipe.id}: #{recipe.name} #{recipe.url}"
         end
